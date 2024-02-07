387a8da19e911e8c7aa56a560d9d10b8e6d425bc
KitagawaKouta <kkitagawa16219@gmail.com>
2024-02-08 01:33:09 +0900

    gg

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="Kiwi">
+    <NuxtLayout />
+    <NuxtPage />
   </div>
 </template>
+<style>
+.Kiwi {
+  font-family: "Kiwi Maru", serif;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
+<script lang="ts" setup>
+import "~/assets/css/default.css";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: [
+    "@vueuse/nuxt",
+    "usebootstrap",
+    "usebootstrap-docs",
+    "nuxt-icon",
+    "@nuxt/image",
+  ],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      title: "就活アシスタント！",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          // TODO: ここに先ほど控えたhrefの中身を記載
+          href: "https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap",
+          crossorigin: "",
+        },
+      ],
+    },
+  },
+  css: ["bootstrap/scss/bootstrap.scss"],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
